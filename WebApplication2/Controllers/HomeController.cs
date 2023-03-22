ace750a2cdd530473620b04d5799baea2e723b99
MohammadJS2642 <mohammadjs2642@gmail.com>
2023-03-22 13:13:32 +0430

    removed a project and added mvc project and using another package of github package in this mvc project

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+using MyPackageDevelopment;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var text = MyPackageDevelopment.MyPackageDevelopment.SomeText();
+            ViewData["text"] = text;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
